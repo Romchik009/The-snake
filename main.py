7bb683bdb9e1225df1a479f5b1c7179544ad5ba2
Roma <166724833+Romchik009@users.noreply.github.com>
2024-11-07 17:46:30 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+import time
+import random
+
+pygame.init():
+
+white = (255, 255, 255)
+yellow = (255, 255, 102)
+red = (255, 0, 0)
+black = (0, 0, 0)
+green = (137, 172, 118)
+blue = (0, 255, 255)
+
+dis_widht = 600
+dis_height = 500
+
+
+
+
+*пока что так*
